f61d3b7fee68e1da100d94d020f976ba24b66e86
ehangehang <rayhansuryatama@gmail.com>
2023-06-30 16:44:57 +0700

    score timer and bird fix

--- src/components/objects/bird/ObjectBird.js
+++ src/components/objects/bird/ObjectBird.js
@@ -5,27 +5,23 @@ import './Bird.css';
 function Bird() {
     const [positionX, setPositionX] = useState(-100);
     const [positionY, setPositionY] = useState(0);
-    // masih glitching, harusnya burung pindah posisi y setelah x keyframe selesai
-    const [isFinished, setIsFinished] = useState(true);
     const containerWidth = 1024;
     const speed = 1;
 
     useEffect(() => {
         const timer = setInterval(() => {
-            setIsFinished(false);
             setPositionX((prevPositionX) => 
             (prevPositionX + speed) % containerWidth);
+
+            if (positionX <= 0) {
+                setPositionY(Math.floor(Math.random() * containerWidth) + 1);
+            }
         });
 
         return () => {
             clearInterval(timer);
-            setIsFinished(true);
         };
     });
-
-    useEffect(() => {
-        setPositionY(Math.floor(Math.random() * containerWidth) + 1);
-    }, [isFinished]);
 
     return (
         <img src={bird} alt="bird" className="bird" style={{right: positionX, top: positionY}}/>
--- src/components/objects/hud/ObjectHud.js
+++ src/components/objects/hud/ObjectHud.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './Hud.css';
+import Timer from "react-timer-wrapper";
+import Timecode from "react-timecode";
 
 function Hud() {
+    const [time, setTime] = useState(0);
+    const [start, setStart] = useState(false);
+
+    useEffect(() => {
+        const handleEvent = (e) => {
+            switch (e.key) {
+                case "ArrowUp":
+                    setStart(true);
+                    break;
+                case "ArrowDown":
+                    setStart(true);
+                    break;
+                case "ArrowLeft":
+                    setStart(true);
+                    break;
+                case "ArrowRight":
+                    setStart(true);
+                    break;
+            }
+        }
+
+        window.addEventListener("keydown", handleEvent);
+
+        let interval = null;
+
+        if (start) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 10);
+            }, 10);
+        } else {
+            clearInterval(interval);
+        }
+
+        return () => {
+            window.removeEventListener("keydown", handleEvent);
+            clearInterval(interval);
+        }
+    }, [start]);
+
     return (
-        <div className="hud">
-            <h1 className="hud-score">Score: 0</h1>
-            <h1 className="hud-time">Time: 0s</h1>
+        <div className="hud hud-time">
+            <Timer active={start} duration={null}>
+                <Timecode time={time} format="s.SS" prefix="Time: " postfix="s"/>
+            </Timer>
         </div>
     );
 }
